55e60344461581d7f7ef130329bf3ecca13e2414
Jacob Blakely <jacob@codekirei.com>
2016-03-23 01:50:36 -0700

    refactor switch statement to object lookup

--- src/containers/todo-list-container.js
+++ src/containers/todo-list-container.js
@@ -3,22 +3,25 @@ import { toggleTodo } from '../actions'
 import { SHOW_ALL, SHOW_COMPLETED, SHOW_ACTIVE } from '../constants/filters'
 import TodoList from '../components/todo-list.jsx'
 
-const getVisibleTodos = (todos, filter) => {
-  switch (filter) {
-
-    case SHOW_ALL:
-      return todos
-
-    case SHOW_COMPLETED:
-      return todos.filter(t => t.completed)
-
-    case SHOW_ACTIVE:
-      return todos.filter(t => !t.completed)
-
-    default:
-      return todos
-  }
-}
+const getVisibleTodos = (todos, filter) => ({
+  [SHOW_ALL]: todos,
+  [SHOW_COMPLETED]: todos.filter(todo => todo.completed),
+  [SHOW_ACTIVE]: todos.filter(todo => !todo.completed),
+  // switch (filter) {
+  //
+  //   case SHOW_ALL:
+  //     return todos
+  //
+  //   case SHOW_COMPLETED:
+  //     return todos.filter(t => t.completed)
+  //
+  //   case SHOW_ACTIVE:
+  //     return todos.filter(t => !t.completed)
+  //
+  //   default:
+  //     return todos
+  // }
+})[filter]
 
 export default connect(
   state => ({
