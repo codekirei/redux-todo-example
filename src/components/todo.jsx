56bff9b9c5127fefff1d1975260f681ea08e91e5
Jacob Blakely <jacob@codekirei.com>
2016-03-24 16:14:32 -0700

    move todo strikethrough to css

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,16 +1,13 @@
 import React, { PropTypes as types } from 'react'
 
-const Todo = ({ handleClick, completed, text }) => {
-  const textDecoration = completed ? 'line-through' : 'none'
-  return (
-    <li
-      onClick={handleClick}
-      style={{ textDecoration }}
-    >
-      {text}
-    </li>
-  )
-}
+const Todo = ({ handleClick, completed, text }) => (
+  <li
+    className={completed ? 'todo--completed' : ''}
+    onClick={handleClick}
+  >
+    {text}
+  </li>
+)
 
 Todo.propTypes = {
   handleClick: types.func.isRequired,
