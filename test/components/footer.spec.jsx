b3eff5f52b9605a6bdd67e3774fd733bd762255a
Jacob Blakely <jacob@codekirei.com>
2016-03-29 13:47:10 -0700

    update todo

---
+++ test/components/footer.spec.jsx
@@ -0,0 +1,12 @@
+// test modules --------------------------------------------
+
+import React from 'react'
+import { shallow } from 'enzyme'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+chai.use(chaiEnzyme())
+
+// test cases ----------------------------------------------
+
+import Footer from '../../src/components/footer.jsx'
+// const component = shallow(<Footer />)
---
+++ test/components/link.spec.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import chai, { expect } from 'chai'
+import chaiEnzyme from 'chai-enzyme'
+chai.use(chaiEnzyme())
+
+import Link from '../../src/components/link.jsx'
+
+// exports['<Link />'] = {
+//
+// }
