209658f4e3d9b1674b5e85c88dd8ad7265d10f58
Jacob Blakely <jacob@codekirei.com>
2016-04-06 12:39:30 -0700

    clean up root component

--- src/components/root.jsx
+++ src/components/root.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
+import { createElement } from 'react'
 import TodoForm from './connected/todo-form'
 import TodoList from './connected/todo-list'
 import Footer from './composed/footer.jsx'
 import DevTools from './composed/devtools.jsx'
 
-export const DevRoot = () => (
-  <div>
-    <TodoForm />
-    <TodoList />
-    <Footer />
-    <DevTools />
-  </div>
-)
+const root = (...children) => createElement('div', { className: 'react' }, ...children)
 
-export const ProductionRoot = () => (
-  <div>
-    <TodoForm />
-    <TodoList />
-    <Footer />
-  </div>
-)
+const children = [
+  TodoForm,
+  TodoList,
+  Footer,
+]
+
+export const DevRoot = () =>
+  root.apply(null, children.concat(DevTools).map(createElement))
+
+export const ProductionRoot = () =>
+  root.apply(null, children.map(createElement))
