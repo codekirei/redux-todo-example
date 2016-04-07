e4dc3491f165e0a99770feb4bbd0e88ee7eba7c0
Jacob Blakely <jacob@codekirei.com>
2016-04-06 17:17:02 -0700

    write tests for utils/filter-todos

---
+++ test/utils/filter-todos.spec.js
@@ -0,0 +1,26 @@
+import { expect } from 'chai'
+import filterTodos from '../../src/utils/filter-todos'
+import {
+  SHOW_ALL,
+  SHOW_COMPLETED,
+  SHOW_ACTIVE,
+} from '../../src/constants/filter-constants'
+
+const todos = [
+  { id: 1, completed: true },
+  { id: 2, completed: false },
+]
+
+exports['filter-todos'] = {
+
+  [SHOW_ALL]: () => expect(filterTodos(todos, SHOW_ALL)).to.deep.equal(todos),
+
+  [SHOW_COMPLETED]: () => expect(filterTodos(todos, SHOW_COMPLETED)).to.deep.equal([
+    { id: 1, completed: true },
+  ]),
+
+  [SHOW_ACTIVE]: () => expect(filterTodos(todos, SHOW_ACTIVE)).to.deep.equal([
+    { id: 2, completed: false },
+  ]),
+
+}
